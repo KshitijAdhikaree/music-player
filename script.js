b5436e8eae8466c94a412b5cf8e23cdd42803406
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-03-17 16:30:51 +0545

    update music files

--- script.js
+++ script.js
@@ -1,61 +1,62 @@
-const image = document.querySelector("img");
-const title = document.getElementById("title");
-const artist = document.getElementById("artist");
-const music = document.querySelector("audio");
-const progressContainer = document.getElementById("progress-container");
-const progress = document.getElementById("progress");
-const currentTimeEl = document.getElementById("current-time");
-const durationEl = document.getElementById("duration");
-const prevBtn = document.getElementById("prev");
-const playBtn = document.getElementById("play");
-const nextBtn = document.getElementById("next");
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const music = document.querySelector('audio');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
 
-//Music
+// Music
 const songs = [
   {
-    name: "jacinto-1",
-    displayName: "Electric Chill Machine",
-    artist: "Jacinto Design",
+    name: 'kshitij-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Kshitij Adhikaree',
   },
   {
-    name: "jacinto-2",
-    displayName: "Seven Nation Army (Remix)",
-    artist: "Jacinto Design",
+    name: 'kshitij-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Kshitij Adhikaree',
   },
   {
-    name: "jacinto-3",
-    displayName: "Goodnight, Disco Queen",
-    artist: "Jacinto Design",
+    name: 'kshitij-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Kshitij Adhikaree',
   },
   {
-    name: "metric-1",
-    displayName: "Front Row (Remix)",
-    artist: "Metric/Jacinto Design",
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Kshitij Adhikaree',
   },
 ];
-//  Check if Playing
+
+// Check if Playing
 let isPlaying = false;
 
 // Play
 function playSong() {
   isPlaying = true;
-  playBtn.classList.replace("fa-play", "fa-pause");
-  playBtn.setAttribute("title", "Pause");
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
   music.play();
 }
 
 // Pause
 function pauseSong() {
   isPlaying = false;
-  playBtn.classList.replace("fa-pause", "fa-play");
-  playBtn.setAttribute("title", "Play");
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
   music.pause();
 }
 
 // Play or Pause Event Listener
-playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 
-//update DOM
+// Update DOM
 function loadSong(song) {
   title.textContent = song.displayName;
   artist.textContent = song.artist;
@@ -63,66 +64,60 @@ function loadSong(song) {
   image.src = `img/${song.name}.jpg`;
 }
 
-//Current Song
+// Current Song
 let songIndex = 0;
 
-//Previous Song
+// Previous Song
 function prevSong() {
   songIndex--;
   if (songIndex < 0) {
     songIndex = songs.length - 1;
   }
-  console.log(songIndex);
   loadSong(songs[songIndex]);
   playSong();
 }
 
-//Next Song
+// Next Song
 function nextSong() {
   songIndex++;
   if (songIndex > songs.length - 1) {
     songIndex = 0;
   }
-  console.log(songIndex);
   loadSong(songs[songIndex]);
   playSong();
 }
-// On load- select first song
+
+// On Load - Select First Song
 loadSong(songs[songIndex]);
 
-//Update Progress Bar and Time
+// Update Progress Bar & Time
 function updateProgressBar(e) {
   if (isPlaying) {
     const { duration, currentTime } = e.srcElement;
-
-    //Update progressBar width
+    // Update progress bar width
     const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
-    //Calculate display for duration
+    // Calculate display for duration
     const durationMinutes = Math.floor(duration / 60);
-
     let durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
       durationSeconds = `0${durationSeconds}`;
     }
-
-    //Delay switching duration element to avoid NaN
+    // Delay switching duration Element to avoid NaN
     if (durationSeconds) {
       durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
     }
-    //Calculate display for current
+    // Calculate display for currentTime
     const currentMinutes = Math.floor(currentTime / 60);
-
     let currentSeconds = Math.floor(currentTime % 60);
     if (currentSeconds < 10) {
       currentSeconds = `0${currentSeconds}`;
     }
-
     currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
   }
 }
 
-//Set Progress Bar
+// Set Progress Bar
 function setProgressBar(e) {
   const width = this.clientWidth;
   const clickX = e.offsetX;
@@ -130,9 +125,9 @@ function setProgressBar(e) {
   music.currentTime = (clickX / width) * duration;
 }
 
-//Event Listeners
-prevBtn.addEventListener("click", prevSong);
-nextBtn.addEventListener("click", nextSong);
-music.addEventListener("ended", nextSong);
-music.addEventListener("timeupdate", updateProgressBar);
-progressContainer.addEventListener("click", setProgressBar);
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+music.addEventListener('timeupdate', updateProgressBar);
+progressContainer.addEventListener('click', setProgressBar);
